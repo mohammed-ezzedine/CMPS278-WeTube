7dc60f760468e08fd94afccc7f658fa9d062ba25
Mohammed Ezzedine <mezdn@outlook.com>
2020-11-11 19:05:33 +0200

    Configure remote database

---
+++ Server/YouTubeClone/Controllers/ChannelController.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using YouTubeClone.Data;
+using YouTubeClone.Models;
+
+namespace YouTubeClone.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ChannelController : ControllerBase
+    {
+        private readonly YouTubeContext context;
+
+        public ChannelController(YouTubeContext context)
+        {
+            this.context = context;
+        }
+
+        // GET: api/Channel
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Channel>>> GetChannel()
+        {
+            var channels = await context.Channel
+                .Include(c => c.Playlists)
+                .ThenInclude(p => p.Videos)
+                .Include(c => c.Videos)
+                .ToListAsync();
+
+            return channels;
+        }
+
+        // GET: api/Channel/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Channel>> GetChannel(int id)
+        {
+            var channel = await context.Channel
+                .Include(c => c.Playlists)
+                .ThenInclude(p => p.Videos)
+                .Include(c => c.Videos)
+                .Include(c => c.Subscribers)
+                .FirstOrDefaultAsync(c => c.Id == id);
+
+            if (channel == null)
+            {
+                return NotFound();
+            }
+
+            return channel;
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<Channel>> PostChannel([FromRoute] string userId, [FromRoute] string userSecret, Channel channel)
+        {
+            var user = await context.User.FindAsync(userId);
+
+            if (user == null || user.Secret != Guid.Parse(userSecret))
+            {
+                return Unauthorized();
+            }
+
+            user.Channel = channel;
+            context.Channel.Add(channel);
+            await context.SaveChangesAsync();
+
+            return channel;
+        }
+    }
+}
--- Server/YouTubeClone/Program.cs
+++ Server/YouTubeClone/Program.cs
@@ -1,11 +1,9 @@
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using YouTubeClone.Data;
 
 namespace YouTubeClone
 {
@@ -13,7 +11,17 @@ namespace YouTubeClone
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            var host = CreateHostBuilder(args).Build();
+
+            using (var scope = host.Services.CreateScope())
+            {
+                var services = scope.ServiceProvider;
+
+                var context = services.GetRequiredService<YouTubeContext>();
+                context.Database.EnsureCreated();
+            }
+
+            host.Run();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
