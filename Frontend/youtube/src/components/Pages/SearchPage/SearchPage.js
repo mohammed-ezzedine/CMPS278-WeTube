de66b2b7536a8c2a9d92bad1dd918ab7f637fe51
firasharb <firas-harb@hotmail.com>
2020-12-04 22:19:13 +0200

    No idea

--- Frontend/youtube/src/components/Pages/SearchPage/SearchPage.js
+++ Frontend/youtube/src/components/Pages/SearchPage/SearchPage.js
@@ -3,6 +3,7 @@ import './SearchPage.css';
 import VideoRow from '../../VideoRow/VideoRow';
 
 import TuneOutlinedIcon from '@material-ui/icons/TuneOutlined';
+import videos from '../../../assets/recommendedVideos.json';
 
 function SearchPage() {
   return (
@@ -16,46 +17,19 @@ function SearchPage() {
         Dummy Data for testing
         Keep there until we start handling real data
        */}
-      <VideoRow
-        views="32M"
-        description="In this video I discuss why I have no interested in machine learning. ---- If you like cooking, checkout my side project: ..."
-        timestamp="1 year ago"
-        channel="Ben Awad"
-        title="Why I don't like Machine Learning"
-        image="http://i3.ytimg.com/vi/HnKaaDcWOXw/maxresdefault.jpg"
-      />
-      <VideoRow
-        views="32M"
-        description="Cats are awesome, and super funny too! Who doesn't like cats and kittens? They make us laugh and happy! Just look how they ..."
-        timestamp="4 years ago"
-        channel="Tiger Productions"
-        title="The funniest and most humorous cat videos ever! - Funny cat compilation"
-        image="http://i3.ytimg.com/vi/gXuU8qgJcYo/maxresdefault.jpg"
-      />
-      <VideoRow
-        views="32M"
-        description="Cats are awesome, and super funny too! Who doesn't like cats and kittens? They make us laugh and happy! Just look how they ..."
-        timestamp="4 years ago"
-        channel="Tiger Productions"
-        title="The funniest and most humorous cat videos ever! - Funny cat compilation"
-        image="http://i3.ytimg.com/vi/VPVzx1ZOVuw/maxresdefault.jpg"
-      />
-      <VideoRow
-        views="32M"
-        description="Cats are awesome, and super funny too! Who doesn't like cats and kittens? They make us laugh and happy! Just look how they ..."
-        timestamp="4 years ago"
-        channel="Tiger Productions"
-        title="The funniest and most humorous cat videos ever! - Funny cat compilation"
-        image="http://i3.ytimg.com/vi/rhPSo4_Tgi0/maxresdefault.jpg"
-      />
-      <VideoRow
-        views="32M"
-        description="Cats are awesome, and super funny too! Who doesn't like cats and kittens? They make us laugh and happy! Just look how they ..."
-        timestamp="4 years ago"
-        channel="Tiger Productions"
-        title="The funniest and most humorous cat videos ever! - Funny cat compilation"
-        image="http://i3.ytimg.com/vi/NT299zIk2JY/maxresdefault.jpg"
-      />
+
+      {videos.map((video) => {
+        return (
+          <VideoRow
+            views={video.views}
+            description={video.description}
+            timestamp={video.timestamp}
+            channel={video.channel}
+            title={video.title}
+            image={video.image}
+          />
+        );
+      })}
     </div>
   );
 }
