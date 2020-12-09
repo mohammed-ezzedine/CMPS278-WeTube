adff7ea45436f4253703099a1cb4252b9cc90368
Mohammed Ezzedine <mezdn@outlook.com>
2020-12-09 21:29:58 +0200

    Add Recommended videos

--- Frontend/youtube/src/components/Pages/RecommendedVideos/RecommendedVideos.js
+++ Frontend/youtube/src/components/Pages/RecommendedVideos/RecommendedVideos.js
@@ -1,11 +1,21 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './RecommendedVideos.css';
 import VideoCard from '../../VideoCard/VideoCard';
 
-import videos from '../../../assets/recommendedVideos.json';
-
 function RecommendedVideos() {
-  console.log(videos);
+  const [videos, setvideos] = useState([]);
+  
+  useEffect(() => {
+    fetch("https://youtube278.azurewebsites.net/api/video/recommendation", {
+      method: "GET",
+      headers: {
+        "Access-Control-Allow-Origin": "*"
+      }
+    })
+    .then (r => r.json())
+    .then (d => setvideos(d));
+  }, []);
+  
   return (
     <div className="recommendedVideos">
       <h2>Latest</h2>
@@ -14,11 +24,11 @@ function RecommendedVideos() {
           return (
             <VideoCard
               title={video.title}
-              views={video.views}
-              timestamp={video.timestamp}
-              channelImg={video.channelImg}
-              channel={video.channel}
-              image={video.image}
+              views={video.views.length}
+              timestamp=""
+              channelImg={video.author.imageUrl}
+              channel=""
+              image={video.thumbnailUrl}
             />
           );
         })}
