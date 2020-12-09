16c40289a1b07ea9f4592ef27d7adca369f10c09
firasharb <firas-harb@hotmail.com>
2020-12-09 19:04:08 +0200

    Added some stuff

--- Frontend/youtube/src/components/Forms/Channel/styles.js
+++ Frontend/youtube/src/components/Forms/Channel/styles.js
@@ -1,37 +1,41 @@
-import React from 'react'
+import React from 'react';
 import { makeStyles } from '@material-ui/core';
 
-
-
-
 export const useEditChannelStyles = makeStyles((theme) => ({
-    paper: {
-      marginTop: theme.spacing(8),
-      display: 'flex',
-      flexDirection: 'column',
-      alignItems: 'center',
-    },
-    paperLessMargin: {
-      marginTop: theme.spacing(4),
-      display: 'flex',
-      flexDirection: 'column',
-      alignItems: 'center',
-    },
-    avatar: {
-      margin: theme.spacing(1),
-      backgroundColor: theme.palette.secondary.main,
-    },
-    form: {
-      width: '100%', // Fix IE 11 issue.
-      marginTop: theme.spacing(3),
-    },
-    submit: {
-      margin: theme.spacing(3, 0, 2),
-    },
-    textarea: {
-      resize: "both"
-    },
-    marginBottom:{
-      marginBottom: theme.spacing(3),
-    }
-  }));
+  paper: {
+    marginTop: theme.spacing(8),
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+  },
+  paperLessMargin: {
+    marginTop: theme.spacing(4),
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+  },
+  avatar: {
+    margin: theme.spacing(1),
+    backgroundColor: theme.palette.secondary.main,
+  },
+  form: {
+    width: '100%', // Fix IE 11 issue.
+    marginTop: theme.spacing(3),
+  },
+  submit: {
+    margin: theme.spacing(3, 0, 2),
+  },
+  textarea: {
+    resize: 'both',
+  },
+  marginBottom: {
+    marginBottom: theme.spacing(3),
+  },
+  image: {
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+    width: '100%',
+    padding: '30px 0',
+  },
+}));
--- Frontend/youtube/src/components/Pages/AddVideo/AddVideo.js
+++ Frontend/youtube/src/components/Pages/AddVideo/AddVideo.js
@@ -1,8 +1,8 @@
-import React, { useState } from "react";
-import { Formik, Form, Field, ErrorMessage, useFormik } from "formik";
-import VideoCard from "../../VideoCard/VideoCard";
+import React, { useState } from 'react';
+import { Formik, Form, Field, ErrorMessage, useFormik } from 'formik';
+import VideoCard from '../../VideoCard/VideoCard';
 
-import "./AddVideo.css";
+import './AddVideo.css';
 import {
   Avatar,
   Box,
@@ -13,19 +13,19 @@ import {
   Snackbar,
   TextField,
   Typography,
-} from "@material-ui/core";
-import Alert from "@material-ui/lab/Alert";
-import VideoCallTwoToneIcon from "@material-ui/icons/VideoCallTwoTone";
+} from '@material-ui/core';
+import Alert from '@material-ui/lab/Alert';
+import VideoCallTwoToneIcon from '@material-ui/icons/VideoCallTwoTone';
 
-import * as yup from "yup";
-import { useStyles } from "../../Forms/Register/styles";
-import { Redirect } from "react-router-dom";
-import Copyright from "../../Forms/Register/Copyright";
+import * as yup from 'yup';
+import { useStyles } from '../../Forms/Register/styles';
+import { Redirect } from 'react-router-dom';
+import Copyright from '../../Forms/Register/Copyright';
 
 const AddVideo = () => {
-  const [Title, setTitle] = useState("");
-  const [Description, setDescription] = useState("");
-  const [errorMessage, seterrorMessage] = useState("");
+  const [Title, setTitle] = useState('');
+  const [Description, setDescription] = useState('');
+  const [errorMessage, setErrorMessage] = useState('');
   const [Thumbnail, setThumbnail] = useState(null);
   const maxNumber = 1;
   const [Video, setVideo] = useState(null);
@@ -33,7 +33,7 @@ const AddVideo = () => {
   const [success, setSuccess] = useState(false);
   const classes = useStyles();
   const handleClose = (event, reason) => {
-    if (reason === "clickaway") {
+    if (reason === 'clickaway') {
       return;
     }
 
@@ -47,15 +47,15 @@ const AddVideo = () => {
   const validationSchema = yup.object({
     Title: yup
       .string()
-      .min(2, "Title must contain at least 2 characters")
-      .max(100, "Title must contain max 100 characters")
-      .required("Title is required"),
+      .min(2, 'Title must contain at least 2 characters')
+      .max(100, 'Title must contain max 100 characters')
+      .required('Title is required'),
     Description: yup.string(),
   });
   const formik = useFormik({
     initialValues: {
-      Title: Title || "",
-      Description: Description || "",
+      Title: Title || '',
+      Description: Description || '',
       Thumbnail: Thumbnail || null,
       Video: Video || null,
     },
@@ -63,10 +63,42 @@ const AddVideo = () => {
     onSubmit: async (values) => {
       await setTimeout(async () => {
         try {
-          
+          const currentUser = JSON.parse(
+            window.localStorage.getItem('CurrentUser')
+          );
+
+          //TODO: implement creating video
+          const requestOptions = {
+            method: 'PUT',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify({
+              video: [{}],
+              thubmnail: [{}],
+              userId: currentUser.id,
+              userSecret: currentUser.secret,
+              shown: true,
+              title: values.Title,
+              description: values.Description,
+            }),
+          };
+          fetch(
+            'https://youtube278.azurewebsites.net/api/Video',
+            requestOptions
+          );
+          console.log(values);
+
+          setErrorMessage('Video Uploaded Successfully');
+          setSuccess(true);
+          setOpen(true);
+
+          //TODO: implement update video
+
+          setErrorMessage('Video Uploaded Successfully');
+          setSuccess(true);
+          setOpen(true);
         } catch (error) {
-          seterrorMessage(
-            "Critical error occured, check your internet connection"
+          setErrorMessage(
+            'Critical error occured, check your internet connection'
           );
           setSuccess(false);
           setOpen(true);
@@ -76,80 +108,77 @@ const AddVideo = () => {
   });
 
   return (
-      <Container component="main" maxWidth="xs">
-        <CssBaseline />
-        <Snackbar open={open} autoHideDuration={6000} onClose={handleClose}>
-          <Alert onClose={handleClose} severity={success ? "success" : "error"}>
-            {errorMessage}
-          </Alert>
-        </Snackbar>
-        <div className={classes.paper}>
-          <Avatar className={classes.avatar}>
-            <VideoCallTwoToneIcon />
-          </Avatar>{" "}
-          <Typography component="h1" variant="h5">
-            Upload{" "}
-          </Typography>{" "}
-          <form className={classes.form} onSubmit={formik.handleSubmit}>
-            <Grid container spacing={3}>
-              <Grid item xs={12}>
-                <TextField
-                  variant="outlined"
-                  required
-                  fullWidth
-                  id="title"
-                  label="Title"
-                  name="Title"
-                  value={formik.values.Title}
-                  onChange={formik.handleChange}
-                  error={formik.touched.Title && Boolean(formik.errors.Title)}
-                  helperText={formik.touched.Title && formik.errors.Title}
-                />
-              </Grid>
-              <Grid item xs={12}>
-                <TextField
-                  variant="outlined"
-                  required
-                  fullWidth
-                  rows={2}
-                  name="Description"
-                  label="Description"
-                  type="text"
-                  id="description"
-                  multiline
-                  value={formik.values.Description}
-                  onChange={formik.handleChange}
-                  error={
-                    formik.touched.Description &&
-                    Boolean(formik.errors.Description)
-                  }
-                  helperText={
-                    formik.touched.Description && formik.errors.Description
-                  }
-                  inputProps={{ className: classes.textarea }}
-                />
-              </Grid>{" "}
-              <Grid item xs={12}>
-                  
-
-              </Grid>
-            </Grid>{" "}
-            <Button
-              type="submit"
-              fullWidth
-              variant="contained"
-              color="primary"
-              className={classes.submit}
-            >
-              Upload{" "}
-            </Button>{" "}
-          </form>{" "}
-        </div>{" "}
-        <Box mt={5}>
-          <Copyright />
-        </Box>{" "}
-        {success ? <Redirect to="../../Pages/RecommendedVideos" /> : ""}
-      </Container>
+    <Container component="main" maxWidth="xs">
+      <CssBaseline />
+      <Snackbar open={open} autoHideDuration={6000} onClose={handleClose}>
+        <Alert onClose={handleClose} severity={success ? 'success' : 'error'}>
+          {errorMessage}
+        </Alert>
+      </Snackbar>
+      <div className={classes.paper}>
+        <Avatar className={classes.avatar}>
+          <VideoCallTwoToneIcon />
+        </Avatar>{' '}
+        <Typography component="h1" variant="h5">
+          Upload{' '}
+        </Typography>{' '}
+        <form className={classes.form} onSubmit={formik.handleSubmit}>
+          <Grid container spacing={3}>
+            <Grid item xs={12}>
+              <TextField
+                variant="outlined"
+                required
+                fullWidth
+                id="title"
+                label="Title"
+                name="Title"
+                value={formik.values.Title}
+                onChange={formik.handleChange}
+                error={formik.touched.Title && Boolean(formik.errors.Title)}
+                helperText={formik.touched.Title && formik.errors.Title}
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <TextField
+                variant="outlined"
+                required
+                fullWidth
+                rows={2}
+                name="Description"
+                label="Description"
+                type="text"
+                id="description"
+                multiline
+                value={formik.values.Description}
+                onChange={formik.handleChange}
+                error={
+                  formik.touched.Description &&
+                  Boolean(formik.errors.Description)
+                }
+                helperText={
+                  formik.touched.Description && formik.errors.Description
+                }
+                inputProps={{ className: classes.textarea }}
+              />
+            </Grid>{' '}
+            <Grid item xs={12}></Grid>
+          </Grid>{' '}
+          <Button
+            type="submit"
+            fullWidth
+            variant="contained"
+            color="primary"
+            className={classes.submit}
+          >
+            Upload{' '}
+          </Button>{' '}
+        </form>{' '}
+      </div>{' '}
+      <Box mt={5}>
+        <Copyright />
+      </Box>{' '}
+      {/* {success ? <Redirect to="../../Pages/RecommendedVideos" /> : ''} */}
+    </Container>
   );
 };
 
