a1ffed3a4925e26ba3dce32efb75b3fc2cdef33d
firasharb <firas-harb@hotmail.com>
2020-12-11 17:20:36 +0200

    Minor Bug Fixes

--- Frontend/youtube/src/components/Pages/YourVideosPage/YourVideosPage.js
+++ Frontend/youtube/src/components/Pages/YourVideosPage/YourVideosPage.js
@@ -9,23 +9,30 @@ function YourVideosPage() {
   const currentUser = JSON.parse(window.localStorage.getItem('CurrentUser'));
 
   useEffect(() => {
-    fetch(`https://youtube278.azurewebsites.net/api/video/channel/${currentUser.channel.id}`)
-      .then(response => response.json())
-      .then(result => setVideos(result))
-      .catch(error => console.log('error', error));
+    fetch(
+      `https://youtube278.azurewebsites.net/api/video/channel/${currentUser.channel.id}`
+    )
+      .then((response) => response.json())
+      .then((result) => setVideos(result))
+      .catch((error) => console.log('error', error));
 
-    var raw = JSON.stringify({"userId":currentUser?.id,"userSecret":currentUser?.secret});
-    fetch(`https://youtube278.azurewebsites.net/api/channel/hidden?id=${currentUser.channel.id}`,
-    {
-      "method": 'POST',
-      headers: {
-        "content-type": 'application/json',
-      },
-      body: raw
-    })
-      .then(response => response.json())
-      .then(result => setHiddenVideos(result))
-      .catch(error => console.log('error', error));
+    var raw = JSON.stringify({
+      userId: currentUser?.id,
+      userSecret: currentUser?.secret,
+    });
+    fetch(
+      `https://youtube278.azurewebsites.net/api/channel/hidden?id=${currentUser.channel.id}`,
+      {
+        method: 'POST',
+        headers: {
+          'content-type': 'application/json',
+        },
+        body: raw,
+      }
+    )
+      .then((response) => response.json())
+      .then((result) => setHiddenVideos(result))
+      .catch((error) => console.log('error', error));
   }, [currentUser.id]);
 
   return (
@@ -33,35 +40,43 @@ function YourVideosPage() {
       <h2>Your Videos</h2>
       <div className="yourVideos__videos">
         {videos.map((video) => {
-            return (
-              <VideoCard
-                title={video.title}
-                views={video.views?.length}
-                path={`/video/${video.id}`}
-                timestamp={video.uploadDate.split('T')[0] + " | " + video.uploadDate.split('T')[1].split('.')[0]}
-                channelImg={`https://youtube278.azurewebsites.net/api/channel/image-stream/${video.author.id}`}
-                channel={video.author.name}
-                image={`https://youtube278.azurewebsites.net/api/video/image-stream/${video.id}`}
-              />
-            );
-          })}
+          return (
+            <VideoCard
+              title={video.title}
+              views={video.views?.length}
+              path={video.id}
+              timestamp={
+                video.uploadDate.split('T')[0] +
+                ' | ' +
+                video.uploadDate.split('T')[1].split('.')[0]
+              }
+              channelImg={`https://youtube278.azurewebsites.net/api/channel/image-stream/${video.author.id}`}
+              channel={video.author.name}
+              image={`https://youtube278.azurewebsites.net/api/video/image-stream/${video.id}`}
+            />
+          );
+        })}
       </div>
 
       <h2>Hidden Videos</h2>
       <div className="yourVideos__videos">
         {hiddenVideos.map((video) => {
-            return (
-              <VideoCard
-                title={video.title}
-                views={video.views?.length}
-                path={`/video/${video.id}`}
-                timestamp={video.uploadDate.split('T')[0] + " | " + video.uploadDate.split('T')[1].split('.')[0]}
-                channelImg={`https://youtube278.azurewebsites.net/api/channel/image-stream/${video.author?.id}`}
-                channel={video.author?.name}
-                image={`https://youtube278.azurewebsites.net/api/video/image-stream/${video.id}`}
-              />
-            );
-          })}
+          return (
+            <VideoCard
+              title={video.title}
+              views={video.views?.length}
+              path={video.id}
+              timestamp={
+                video.uploadDate.split('T')[0] +
+                ' | ' +
+                video.uploadDate.split('T')[1].split('.')[0]
+              }
+              channelImg={`https://youtube278.azurewebsites.net/api/channel/image-stream/${video.author?.id}`}
+              channel={video.author?.name}
+              image={`https://youtube278.azurewebsites.net/api/video/image-stream/${video.id}`}
+            />
+          );
+        })}
       </div>
     </div>
   );
