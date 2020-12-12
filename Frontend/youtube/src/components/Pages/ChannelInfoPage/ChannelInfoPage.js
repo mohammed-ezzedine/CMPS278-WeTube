5c96a7f266bad11c5fe42b4ac38b2bdfab6532fa
firasharb <firas-harb@hotmail.com>
2020-12-12 17:55:53 +0200

    Finished static look of channel page

--- Frontend/youtube/src/components/Pages/ChannelInfoPage/ChannelInfoPage.js
+++ Frontend/youtube/src/components/Pages/ChannelInfoPage/ChannelInfoPage.js
@@ -1,27 +1,169 @@
-import React, { useEffect, useState } from 'react'
-import { useParams } from 'react-router-dom'
-import {get} from 'axios';
+import React from 'react';
+import VideoCard from '../../VideoCard/VideoCard';
+import ScrollMenu from 'react-horizontal-scrolling-menu';
+import Avatar from '@material-ui/core/Avatar';
+
+import './ChannelInfoPage.css';
+import { Button } from '@material-ui/core';
 
 function ChannelInfoPage() {
-
-    const {id} = useParams();
-    const [channel, setChannel] = useState({})
-
-    useEffect(() => {
-        
-        fetch(`https://youtube278.azurewebsites.net/api/channel/${id}`)
-        .then(response => response.json())
-        .then(channelJSON => setChannel(channelJSON))
-        .catch(error => console.error(error));
-        
-        
-    }, [id])
-
-    return (
-        <div>
-            {JSON.stringify(channel)}
+  return (
+    <div className="channelInfo">
+      <div className="channelInfo__interactive">
+        <Avatar className="channelInfo__userAvatar" />
+        <div className="channelInfo__interactiveInfo">
+          <div className="channelInfo__interactiveInfoLeft">
+            <h4>Test</h4>
+            <p>317k subscribers</p>
+          </div>
+          <div className="channelInfo__interactiveInfoRight">
+            <Button
+              variant="contained"
+              style={{ backgroundColor: '#c00', color: 'white' }}
+            >
+              Subscribe
+            </Button>
+          </div>
         </div>
-    )
+      </div>
+      <div className="channelInfo__featured">
+        <h2>Featured</h2>
+        <hr style={{ margin: '5px 0' }} />
+        <div className="channelInfo__featuredContainer">
+          {/* <ScrollMenu></ScrollMenu> */}
+          <ScrollMenu
+            arrowLeft={<div style={{ fontSize: '30px' }}>{' < '}</div>}
+            arrowRight={<div style={{ fontSize: '30px' }}>{' > '}</div>}
+            data={[
+              <VideoCard
+                title="Test"
+                views="12,000"
+                timestamp="2020-02-10"
+                channelImg="./dog.jpg"
+                channel="Firas Harb"
+                image="./dog.jpg"
+              />,
+              <VideoCard
+                title="Test"
+                views="12,000"
+                timestamp="2020-02-10"
+                channelImg="./dog.jpg"
+                channel="Firas Harb"
+                image="./dog.jpg"
+              />,
+              <VideoCard
+                title="Test"
+                views="12,000"
+                timestamp="2020-02-10"
+                channelImg="./dog.jpg"
+                channel="Firas Harb"
+                image="./dog.jpg"
+              />,
+              <VideoCard
+                title="Test"
+                views="12,000"
+                timestamp="2020-02-10"
+                channelImg="./dog.jpg"
+                channel="Firas Harb"
+                image="./dog.jpg"
+              />,
+            ]}
+          />
+        </div>
+      </div>
+      <div className="channelInfo__playlists">
+        <h2>Playlists</h2>
+
+        <hr style={{ margin: '5px 0' }} />
+
+        <div className="channelInfo__playlistsContainer">
+          <ScrollMenu
+            arrowLeft={<div style={{ fontSize: '30px' }}>{' < '}</div>}
+            arrowRight={<div style={{ fontSize: '30px' }}>{' > '}</div>}
+            data={[
+              <VideoCard
+                title="Test"
+                views="12,000"
+                timestamp="2020-02-10"
+                channelImg="./dog.jpg"
+                channel="Firas Harb"
+                image="./dog.jpg"
+              />,
+              <VideoCard
+                title="Test"
+                views="12,000"
+                timestamp="2020-02-10"
+                channelImg="./dog.jpg"
+                channel="Firas Harb"
+                image="./dog.jpg"
+              />,
+              <VideoCard
+                title="Test"
+                views="12,000"
+                timestamp="2020-02-10"
+                channelImg="./dog.jpg"
+                channel="Firas Harb"
+                image="./dog.jpg"
+              />,
+              <VideoCard
+                title="Test"
+                views="12,000"
+                timestamp="2020-02-10"
+                channelImg="./dog.jpg"
+                channel="Firas Harb"
+                image="./dog.jpg"
+              />,
+            ]}
+          />
+        </div>
+      </div>
+      <div className="channelInfo__videos">
+        <h2>All Videos</h2>
+        <hr style={{ margin: '5px 0' }} />
+
+        <div className="channelInfo__videosContainer">
+          <ScrollMenu
+            arrowLeft={<div style={{ fontSize: '30px' }}>{' < '}</div>}
+            arrowRight={<div style={{ fontSize: '30px' }}>{' > '}</div>}
+            data={[
+              <VideoCard
+                title="Test"
+                views="12,000"
+                timestamp="2020-02-10"
+                channelImg="./dog.jpg"
+                channel="Firas Harb"
+                image="./dog.jpg"
+              />,
+              <VideoCard
+                title="Test"
+                views="12,000"
+                timestamp="2020-02-10"
+                channelImg="./dog.jpg"
+                channel="Firas Harb"
+                image="./dog.jpg"
+              />,
+              <VideoCard
+                title="Test"
+                views="12,000"
+                timestamp="2020-02-10"
+                channelImg="./dog.jpg"
+                channel="Firas Harb"
+                image="./dog.jpg"
+              />,
+              <VideoCard
+                title="Test"
+                views="12,000"
+                timestamp="2020-02-10"
+                channelImg="./dog.jpg"
+                channel="Firas Harb"
+                image="./dog.jpg"
+              />,
+            ]}
+          />
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default ChannelInfoPage
+export default ChannelInfoPage;
